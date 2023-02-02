6ca4d42c7908c288e98ed3d2871646957597b272
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-02 21:13:00 +0530

    [Ajay] add : UC1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("UC1");
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class UC1
+    {
+        public static void mymethodUC1()
+        {
+            Console.WriteLine("UC1");
+        }
+
+       
+        }
+    }
+
